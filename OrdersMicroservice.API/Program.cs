172a595df272babbcffa562487e94138e4b953f4
Manoj Jaiswal <manoj.jaiswal883@gmail.com>
2026-06-08 13:02:41 +0530

    fixed the package issue

--- OrdersMicroservice.API/Program.cs
+++ OrdersMicroservice.API/Program.cs
@@ -1,12 +1,42 @@
 using BusinessLogicLayer;
 using DataAccessLayer;
+using FluentValidation.AspNetCore;
+using OrdersMicroservice.API.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDataAccessLayer();
 builder.Services.AddBusinessLogicAccessLayer();
+builder.Services.AddControllers();
+//FluentValidation
+builder.Services.AddFluentValidationAutoValidation();
+
+//Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//Cors
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+    {
+        builder.WithOrigins("http://localhost:4200")
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+    });
+});
 var app = builder.Build();
+app.UseExceptionHandlingMiddleware();
+app.UseRouting();
+app.UseCors();
+app.UseSwagger();
+app.UseSwaggerUI();
 
-app.MapGet("/", () => "Hello World!");
+//Auth
+app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseAuthorization();
 
+//Endpoint
+app.MapControllers();
 app.Run();
