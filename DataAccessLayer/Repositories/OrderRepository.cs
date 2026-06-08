88a6b6b9111e1a917b3de30c77a848eafe49c219
Manoj Kumar Jaiswal <75155083+mjtech-xd@users.noreply.github.com>
2026-06-08 15:58:38 +0530

    Implement Order repository with CRUD operations and dependency injection (#2)

---
+++ DataAccessLayer/Repositories/OrderRepository.cs
@@ -0,0 +1,55 @@
+using DataAccessLayer.Entities;
+using DataAccessLayer.RepositoryContracts;
+using MongoDB.Driver;
+
+namespace DataAccessLayer.Repositories;
+
+public class OrderRepository : IOrdersRepository
+{
+    private readonly IMongoCollection<Order> _orders;
+    private readonly string collectionName = "orders";
+    
+    public OrderRepository(IMongoDatabase mongoDatabase)
+    {
+        _orders = mongoDatabase.GetCollection<Order>(collectionName);
+    }
+    public async Task<IEnumerable<Order>> GetOrders()
+    {
+        return (await _orders.FindAsync(Builders<Order>.Filter.Empty)).ToList();
+    }
+
+    public async Task<IEnumerable<Order?>> GetOrdersByCondition(FilterDefinition<Order> filter)
+    {
+        return (await _orders.FindAsync(filter)).ToList();
+    }
+
+    public async Task<Order?> GetOrderByCondition(FilterDefinition<Order> filter)
+    {
+         return (await _orders.FindAsync(filter)).FirstOrDefault();
+    }
+
+    public async Task<Order?> AddOrder(Order order)
+    {
+        order.OrderID = Guid.NewGuid();
+        await _orders.InsertOneAsync(order);
+        return order;
+    }
+
+    public async Task<Order?> UpdateOrder(Order order)
+    {
+        FilterDefinition<Order> filter =  Builders<Order>.Filter.Eq(temp => temp.OrderID, order.OrderID);
+        Order? existingOrder = (await _orders.FindAsync(filter)).FirstOrDefault();
+        if (existingOrder is null)
+            return null;
+        ReplaceOneResult replaceOneResult = await _orders.ReplaceOneAsync(filter, order);
+        return order;
+    }
+
+    public async Task<bool> DeleteOrder(Guid orderID)
+    {
+        FilterDefinition<Order> filter =  Builders<Order>.Filter.Eq(temp => temp.OrderID, orderID);
+        
+        DeleteResult deleteResult = await _orders.DeleteOneAsync(filter);
+        return deleteResult.DeletedCount > 0;
+    }
+}
